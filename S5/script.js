6c71bdafe7851bf0807dc53e731f071bee63ce02
KariDriff <kaioalberth@hotmail.com>
2022-06-15 00:29:54 -0300

    Script 5

---
+++ S5/script.js
@@ -0,0 +1,16 @@
+// Function
+{
+ function funcao() {
+  console.log("Função");
+ }
+
+ funcao();
+
+ // Function com parâmetros
+
+ function msg(parametro1, parametro2) {
+  console.log(parametro1, parametro2)
+ }
+
+ msg("Okay", "Função 2");
+}
